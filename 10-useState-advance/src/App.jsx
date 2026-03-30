5b6a8bbb0cf1645b9f548bd7efb485b35293ef50
Abhay Raj <abhayraj620285@gmail.com>
2026-03-30 23:55:32 +0530

    learned 10-useState

---
+++ 10-useState-advance/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+ const App = () => {
+  const [num, setNum] = useState([10,20,30])
+
+  const btnClicked = () => {
+    const newNum = [...num]
+    newNum.push(99)
+    setNum(newNum)
+
+  }
+  
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={btnClicked}>Click me</button>
+    </div>
+  )
+}
+export default App
